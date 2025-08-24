2124dd23b1ccb2eb0174152603518b21229ec968
ArticlesJoey <joey@articles.media>
2025-08-24 00:55:38 -0400

    Import fix

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import theme from '@/theme';
+import theme from '@/components/theme';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
